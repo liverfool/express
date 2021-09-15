342fa4893df337cd311230c3d9cbad8b2b3cc603
1406MJM <3330kys@dsm.hs.kr>
2021-09-15 17:09:50 +0900

    login

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const item = [{
   height: 175.6
 }];
 
+const ID = {
+  id:'moon',
+  password: '0319'
+};
+
 app.get('/', (req,res) =>{
   res.send("Hello")
 })
@@ -19,14 +24,23 @@ app.get('/user', (req, res) => {
 
 let ages;
 app.post('/user', (req, res) => {
-  if(req.body.age === 17)
+  if(req.body.age === 17) 
   {ages = '1학년'}
   else if(req.body.age === 18)
   {ages = '2학년'}
   else if(req.body.age === 19)
   {ages = '3학년'}
   else{ages = '고등학생이 아닌'}
-
   res.send({msg: `${ages} ${req.body.name}님 반갑습니다`})
 })
+
+app.post('/user/login', (req,res) =>{
+  if(req.body.id == ID.id && req.body.password == ID.password){
+    res.send(`${req.body.id} 님 반갑습니다`)
+  }
+  else {
+    res.send('msg: id or password false')
+  }
+})
+
 app.listen(4000)

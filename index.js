256141ca83b4c70ebbe1083986239683aa279e18
1406MJM <3330kys@dsm.hs.kr>
2021-10-18 22:36:40 +0900

    start connect ejs

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const mysql = require('mysql');
 const dbconfig = require('./database2.js')
 const logger = require('morgan');
 const cookiePaser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const connection = mysql.createConnection(dbconfig);
 const app = express();
 
 //use
 app.use(logger('dev'));
 app.use(cookiePaser());
-
+app.use(bodyParser.urlencoded({extended:true}))
 //get
 app.get('/', (req,res) => {
   res.send('Root');
@@ -36,11 +37,34 @@ app.get('/song', (req,res) => {
   });
 });
 
-app.get('/login', (req,res) => {
+app.get('/id', (req,res) => {
   connection.query('SELECT * from person', (error,rows) => {
     if(error) throw error;
     res.send(rows);
   })
 })
 
+/*
+app.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+app.post('/loginAf', function (req, res, next) {
+  console.log("id : " + req.body.id);
+  console.log("pwd : " + req.body.pwd);
+  console.log("아이디 저장? : " + req.body.rememberId);
+
+  if(req.body.rememberId === "checked"){
+    console.log("아이디 저장 체크!");
+
+    // cookie생성
+    // res.cookie('cookie 이름', 'cookie 내용');
+    res.cookie('loginId', req.body.id);
+  
+    // cookie 읽기
+    console.log(req.cookies);
+  }
+  res.render('home');
+});
+*/
 app.listen(4000);
